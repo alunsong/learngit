c5bb92ef5536302d1156e62d91ea5ab0ad8c905d
alunsong <1678804425@qq.com>
2019-01-03 08:28:19 +0800

    update merge

--- act.py
+++ act.py
@@ -1,10 +1,4 @@
 print("hello world')
-<<<<<<< HEAD
-Creating a new branch is quick and simple.
-=======
-Creating a new branch is quick.
-Creating a new branch is quick AND simplel'
->>>>>>> featurel
 for i in range(0,10):
     print(i)
 def open():
