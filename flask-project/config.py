1aa16650a1cc18d3d853fbb3d16f873f2be61494
alunsong <1678804425@qq.com>
2019-01-07 18:56:41 +0800

    worte a password

--- flask-project/config.py
+++ flask-project/config.py
@@ -13,7 +13,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin 18710032771@163.com'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <18710032771@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
 
